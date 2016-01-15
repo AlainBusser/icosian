4c31c61c577470de6491dedd8218c74720ca4b5e
Alain Busser <alain.busser@gmail.com>
2016-01-15 07:29:50 +0000

    icosien fini

--- javascript/script.js
+++ javascript/script.js
@@ -14,8 +14,6 @@ Utilitaires sous licence MIT
 Alain Busser 2016
 */
 
-/* global Phaser, game, gemmes, chemin */
-
 function addNode(graphe,x,y){                   // ajouter un sommet
     graphe.nodes.push(new Phaser.Point(x,y));
 }
@@ -58,49 +56,8 @@ function contient(tableau,element){             // Le "element in tableau" de Ja
 function liaison(a,b){                          // dit s'il y a une arête entre a et b (numéros des sommets)
     return (contient(chemin.edges[a],b) || contient(chemin.edges[b],a));
 }
-/* global Phaser */
-var game = new Phaser.Game(480,480,Phaser.AUTO,'content',{preload: preload, create: create,update:update});
-var chemin = [ ];                                       // le graphe
-chemin.nodes = [ ];                                     // les sommets
-var gemmes = [ ];                                       // les rubis
-var lieu = 0;
 
-
-var abcs = [240,30,450,130,350,240,94,386,168,312,177,303,142,338,240,205,275,183,297,240];
-var ords = [30,200,200,440,440,100,220,220,382,382,165,165,294,294,368,205,205,270,270,316];
-for (var n in abcs) { addNode(chemin,abcs[n],ords[n]) }
-chemin.edges = [ [1,2,5], [3,6], [4,7], [4,8], [9], 
-    [10,11], [10,12], [11,13], [12,14], [13,14],
-    [15],[16],[17],[18],[19],
-    [16,17],[18],[19],[19],[]
-];
-var possible = [];
-for (var n in abcs){
-    possible[n] = true;
-}
-function essai(acteur,souris) {
-    var oussamissava = prochain(souris.x, souris.y);
-    if(liaison(lieu, oussamissava) && possible[oussamissava]){
-        promenade(chemin.nodes[lieu],chemin.nodes[oussamissava]);
-        lieu = oussamissava;
-    }
-} 
-function manger(){
-    possible[lieu] = false;
-    gemmes[lieu].destroy();
-    gemmes[lieu] = game.add.sprite(chemin.nodes[lieu].x,chemin.nodes[lieu].y,"bouche");
-    gemmes[lieu].anchor.setTo(0.5);
-    gemmes[lieu].scale.setTo(0.3);
-}
-function promenade(P1,P2){ // parcours d'une arête du graphe (d'un point P1 à un autre P2)
-    turtle.x = P1.x;
-    turtle.y = P1.y;
-    turtle.angle = Phaser.Point.angle(P2,P1)*180/Math.PI;
-    P2.angle = turtle.angle;
-    var voyage = game.add.tween(turtle).to(P2,20*P1.distance(P2),"Linear",true);
-    voyage.onComplete.add(manger);
-}
-function prochain(x,y){
+function prochain(x,y){                         // cherche le sommet le plus proche du lieu du clic
     var dmin = 1e6;
     var numero = -1;
     for (var n in chemin.nodes){
@@ -111,8 +68,73 @@ function prochain(x,y){
     }
     return numero;
 }
-var turtle;
-function preload(){   
+
+/* fini pour les graphes, passons au jeu (Ouaaaaaais!) */
+
+
+
+
+/* global Phaser */
+var game = new Phaser.Game(480,480,Phaser.AUTO,'content',{preload: preload, create: create,update:update});
+var chemin = [ ];                                       // le graphe
+chemin.nodes = [ ];                                     // les sommets
+var gemmes = [ ];                                       // les rubis
+var lieu = 0;                                           // où c'est qu'elle est la tortue
+var richesse = 0;                                            // la récolte
+
+// définition du graphe: coordonnées des sommets et définition des arêtes
+
+var abcs = [240,30,450,130,350,240,94,386,168,312,177,303,142,338,240,205,275,183,297,240];
+var ords = [30,200,200,440,440,100,220,220,382,382,165,165,294,294,368,205,205,270,270,316];
+for (var n in abcs) { addNode(chemin,abcs[n],ords[n]) }
+chemin.edges = [ [1,2,5], [3,6], [4,7], [4,8], [9], 
+    [10,11], [10,12], [11,13], [12,14], [13,14],
+    [15],[16],[17],[18],[19],
+    [16,17],[18],[19],[19],[]
+];
+
+
+// le graphe est défini, on fait le jeu
+
+var possible = [];                          // liste des sommets encore visitables
+for (var n in abcs){
+    possible[n] = true;
+}
+
+function essai(acteur,souris) {
+    var oussamissava = prochain(souris.x, souris.y);    // destination visée
+    if(liaison(lieu, oussamissava) && possible[oussamissava]){
+        promenade(chemin.nodes[lieu],chemin.nodes[oussamissava]);
+        lieu = oussamissava;
+    }
+} 
+
+function manger(){                      // ramassage du rubis
+    possible[lieu] = false;             // la tortue ne pourra plus revenir ici
+    if (lieu>0) {
+        gemmes[lieu].destroy();
+        richesse++;
+        aff.text = richesse;
+        gemmes[lieu] = game.add.sprite(chemin.nodes[lieu].x,chemin.nodes[lieu].y,"bouche");
+        gemmes[lieu].anchor.setTo(0.5);
+        gemmes[lieu].scale.setTo(0.3);
+    } else {
+        possible[0] = false;
+        if (richesse == 19) { alert("gagné")}
+    }
+}
+
+function promenade(P1,P2){ // parcours d'une arête du graphe (d'un point P1 à un autre P2)
+    turtle.x = P1.x;
+    turtle.y = P1.y;
+    turtle.angle = Phaser.Point.angle(P2,P1)*180/Math.PI;
+    P2.angle = turtle.angle;
+    var voyage = game.add.tween(turtle).to(P2,20*P1.distance(P2),"Linear",true);
+    voyage.onComplete.add(manger);
+}
+
+var turtle, aff;
+function preload(){                     // à faire avant de faire le jeu
     game.load.image("fond","img/ocean.jpg"); // mettre un masque sur le sable qu'il représente le graphe (en-desous, ocean.jpg)
     //une arète = un rectangle judicieusement tourné
     //un sommet = un disque avec un gaignpi.jpg caché et par-dessus un rubygem.png
@@ -121,9 +143,10 @@ function preload(){
     game.load.spritesheet("bouche","img/gaignpi.png");
     game.load.spritesheet("rubis","img/rubygem.png");
 }
-function create(){ 
+function create(){                      // création du jeu
     var fond = game.add.tileSprite(0,0,game.width,game.height,"fond");
     dessineGraphe(chemin);
+    aff = game.add.text(0,0,0);
     turtle = game.add.sprite(240,30,"tortue");
     turtle.z = 3;
     turtle.anchor.setTo(0.5); // que la tortue soit centrée sur sa case
